c16ad8bc7be8b6374a6ecead4760f4837e58c46e
Teerapat <reetajeen.1960@gmail.com>
2026-02-14 13:20:25 +0700

    valentine website

--- Procfile
+++
@@ -1,1 +0,0 @@
-wed: gunicorn app:app
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def homr():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    
